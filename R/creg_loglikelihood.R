5f816fe39e09bfb541dcf16c8019515033c437a9
Christoph Kiefer <christoph.kiefer@uni-bielefeld.de>
2024-06-13 13:50:02 +0000

    version 0.2-2

--- R/creg_loglikelihood.R
+++ R/creg_loglikelihood.R
@@ -70,7 +70,9 @@ creg_model_estimate <- function(object) {
 
   # Pass start values x and objective function to optimizer
   # TODO: maybe allow for different optimizers
-  fit <- nlminb(x_start, objective_function,
+  fit <- nlminb(
+    start = x_start,
+    objective = objective_function,
     control = list(
       rel.tol = 1e-10,
       eval.max = 200, # 500
@@ -154,6 +156,7 @@ creg_model_objective <- function(datalist, modellist) {
     mu_z <- modellist_g$mu_z
     Sigma_z <- modellist_g$Sigma_z
     Sigma_z_lv <- modellist_g$Sigma_z_lv
+    fixed_z <- modellist_g$fixed_z
     dims <- modellist_g$dims
 
     if (any(!is.na(Sigma_z))) {
@@ -183,7 +186,7 @@ creg_model_objective <- function(datalist, modellist) {
       mu_eta = mu_eta, Sigma_eta = Sigma_eta,
       fixeta = gh_grid$X, ghweight = gh_grid$W,
       mu_z = mu_z, Sigma_z = Sigma_z, Sigma_z_lv = Sigma_z_lv,
-      cores = 1
+      fixed_z = fixed_z, cores = 1
     )
     if (is.na(obj_i)) {
       return(-Inf)
--- src/compute_groupcond_logl.cpp
+++ src/compute_groupcond_logl.cpp
@@ -10,25 +10,24 @@ using namespace Rcpp;
 //'
 //' @param x Model matrix containing the observed variables in order Y, W, Z
 //' @param muy Matrix of conditional expectations of Y given Z and/or latent
-// covariates ' @param sigmayw Vector containing the size parameter (for
-// overdispersion) and measurement error variances ' @param muwz Matrix of
-// conditional expectations of W and Z given latent covariate (if existent) and
-// given the group ' @param sigmaz (Conditional) covariance matrix of observed
-// covariates Z ' @param ghweight Gauss-Hermite weights, if latent covariates
-// included ' @param detvarz Determinant of sigmaz (easier to compute in R...) '
-//@param dims Integer vector with dimensional information, in order group size,
-// integration points, number of Z, and number of W '
+//' covariates ' @param sigmayw Vector containing the size parameter (for
+//' overdispersion) and measurement error variances
+//' @param muwz Matrix of conditional expectations of W and Z given latent
+//' covariate (if existent) and given the group
+//' @param sigmaz (Conditional) covariance matrix of observed covariates Z
+//' @param ghweight Gauss-Hermite weights, if latent covariates included
+//' @param detvarz Determinant of sigmaz (easier to compute in R...)
+//' @param dims Integer vector with dimensional information, in order group
+//' size, integration points, number of Z, and number of W
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
-double compute_groupcond_logl(arma::colvec y, arma::mat w, arma::mat z,
-                              arma::colvec beta, arma::mat Beta,
-                              arma::colvec gamma, arma::mat Gamma,
-                              arma::mat Omega, double overdis, arma::colvec nu,
-                              arma::mat Lambda, arma::mat Theta,
-                              arma::colvec mu_eta, arma::mat Sigma_eta,
-                              arma::mat fixeta, arma::vec ghweight,
-                              arma::colvec mu_z, arma::mat Sigma_z,
-                              arma::mat Sigma_z_lv, int const cores = 1) {
+double compute_groupcond_logl(
+    arma::colvec y, arma::mat w, arma::mat z, arma::colvec beta, arma::mat Beta,
+    arma::colvec gamma, arma::mat Gamma, arma::mat Omega, double overdis,
+    arma::colvec nu, arma::mat Lambda, arma::mat Theta, arma::colvec mu_eta,
+    arma::mat Sigma_eta, arma::mat fixeta, arma::vec ghweight,
+    arma::colvec mu_z, arma::mat Sigma_z, arma::mat Sigma_z_lv, bool fixed_z,
+    int const cores = 1) {
 
 #if defined(_OPENMP)
   omp_set_num_threads(cores);
@@ -68,7 +67,9 @@ double compute_groupcond_logl(arma::colvec y, arma::mat w, arma::mat z,
 
     // Measurement Model part
     arma::mat LamEta = Lambda * GH.t();
+
     arma::mat mu_w = nu + LamEta.each_col();
+
     f_w = dmvnrm_arma_fast(w, mu_w.t(), Theta, false, cores);
   } else {
     // Rcpp::Rcout << "I am here!\n";
@@ -81,9 +82,10 @@ double compute_groupcond_logl(arma::colvec y, arma::mat w, arma::mat z,
 
   // Stochastic covariate part
   arma::colvec f_z(N * N_gh, arma::fill::ones);
-  if (no_lv && no_z) {
+  if (no_lv && no_z && !fixed_z) {
 
     arma::mat GHt = GH.t();
+
     arma::mat tmp =
         Sigma_z_lv * inv_sympd(Sigma_eta) * (GHt.each_col() - mu_eta);
 
@@ -95,7 +97,7 @@ double compute_groupcond_logl(arma::colvec y, arma::mat w, arma::mat z,
       return (R_PosInf);
     }
     f_z = dmvnrm_arma_fast(z, cmu_z, cSigma_z, false, cores);
-  } else if (no_z) {
+  } else if (no_z && !fixed_z) {
     f_z = dmvnrm_arma_fast(z, mu_z.t(), Sigma_z, false, cores);
   }
 
@@ -103,20 +105,34 @@ double compute_groupcond_logl(arma::colvec y, arma::mat w, arma::mat z,
   // beta
   arma::colvec one(N, arma::fill::ones);
   arma::mat zs = arma::join_rows(one, z);
-  arma::colvec mu_y_z = exp(zs * beta);
+  // arma::colvec mu_y_z = exp(zs * beta);
   arma::colvec mu_beta = exp(zs * beta);
 
   // Beta
   arma::colvec mu_Beta(N, arma::fill::ones);
   if (Beta.n_rows) {
-    arma::mat tmp = z * Beta * z.t();
-    mu_Beta = exp(tmp.diag());
+    arma::mat tmp = arma::diagvec(z * Beta * z.t());
+    mu_Beta = exp(tmp);
   }
 
   // gamma
-  arma::colvec mu_y_lv(N_gh, arma::fill::ones);
+  arma::colvec mu_gamma(N_gh, arma::fill::ones);
   if (no_lv) {
-    mu_y_lv = exp(GH * gamma);
+    mu_gamma = exp(GH * gamma);
+  }
+
+  // Gamma
+  arma::colvec mu_Gamma(N, arma::fill::ones);
+  if (Gamma.n_rows) {
+    arma::mat tmp = arma::diagvec(GH * Gamma * GH.t());
+    mu_Gamma = exp(tmp);
+  }
+
+  // Omega
+  arma::mat mu_Omega(N, N_gh, arma::fill::ones);
+  if (Omega.n_rows) {
+    arma::mat tmp = z * Omega * GH.t();
+    mu_Omega = exp(tmp);
   }
 
   arma::colvec f_y;
@@ -124,10 +140,11 @@ double compute_groupcond_logl(arma::colvec y, arma::mat w, arma::mat z,
   if (poisson) {
     // Rcpp::Rcout << "Shortly before Poisson...!\n";
     // sleep(1);
-    f_y = dpois_cpp(y, mu_beta, mu_Beta, mu_y_lv, cores);
+    f_y = dpois_cpp(y, mu_beta, mu_gamma, mu_Beta, mu_Gamma, mu_Omega, cores);
   } else {
     alpha = 1 / overdis;
-    f_y = dnegbin_cpp(y, mu_y_z, mu_y_lv, alpha, cores);
+    f_y = dnegbin_cpp(y, mu_beta, mu_gamma, mu_Beta, mu_Gamma, mu_Omega, alpha,
+                      cores);
   }
 
   // Rcpp::Rcout << "Regression part worked!\n";
